22cab051ba572d561086f2910193ad23043a4fe0
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-12-28 23:12:11 -0800

    accordion practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'title 1',
+    content: 'desc 1',
+  },
+  {
+    title: 'title 2',
+    content: 'desc 2',
+  },
+  {
+    title: 'title 3',
+    content: 'desc 3',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion items={items} />
     </div>
   );
 }
